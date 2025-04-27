acab2543e220535ba8bb99a3988cfa3ff43f4ebc
Dejan Popov <dekipopica92@gmail.com>
2025-04-27 22:33:32 +0200

    tilemap/ tileset work, character movement

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -10,21 +10,21 @@ func _ready() -> void:
 	
 func _process(delta: float) -> void:
 	
-	## Move the player
+	"""Move the player"""
 	var move_vector: Vector2 = Input.get_vector("move_left","move_right","move_up","move_down")
 	velocity = move_vector * move_speed
 	
 	if velocity.x > 0:
 		$AnimatedSprite2D.play("move_right")
-	if velocity.x < 0:
+	elif velocity.x < 0:
 		$AnimatedSprite2D.play("move_left")
-	if velocity.y < 0:
+	elif velocity.y < 0:
 		$AnimatedSprite2D.play("move_up")
-	if velocity.y > 0:
+	elif velocity.y > 0:
 		$AnimatedSprite2D.play("move_down")
-	if velocity == Vector2(0,0):
+	else:
 		$AnimatedSprite2D.stop()
 	move_and_slide()
-	## Move the player
+	"""Move the player"""
 	
 	
